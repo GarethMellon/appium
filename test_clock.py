4f029fc9bf33bb4a7b904bfa0a7903f7b6c0903d
Gareth Mellon <garethmellon@gmail.com>
2019-04-22 19:30:42 +0100

    Added some tests for clock pages

--- test_clock.py
+++ test_clock.py
@@ -1,26 +1,54 @@
 from appium import webdriver
+from appium.webdriver.common.mobileby import MobileBy
+import unittest
 
-desired_cap = {
-    'platformName': 'Android',
-    'platformVersion': '9',
-    'deviceName': 'Android Emulator',
-    'app': "C:\\Users\\User\\Documents\\code\\appium\\apk\\Clock_v6.1.1(238466778)_apkpure.com.apk"
-}
 
-driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_cap)
+class ClockTest(unittest.TestCase):
 
-elemAlarm = driver.find_elements_by_accessibility_id('Alarm')
-elemAlarm[0].click()
-driver.implicitly_wait(1)
+    def setUp(self):
+        desired_cap = {
+            'platformName': 'Android',
+            'platformVersion': '9',
+            'deviceName': 'Android Emulator',
+            'app': "C:\\Users\\User\\Documents\\code\\appium\\apk\\Clock_v6.1.1(238466778)_apkpure.com.apk"
+        }
 
-elemClock = driver.find_elements_by_accessibility_id('Clock')
-elemClock[0].click()
-driver.implicitly_wait(1)
+        self.driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_cap)
 
-elemTimer = driver.find_elements_by_accessibility_id('Timer')
-elemTimer[0].click()
-driver.implicitly_wait(1)
+    def test_clock_alarm(self):
+        self.driver.find_element_by_accessibility_id('Alarm').click()
+        self.driver.find_element_by_accessibility_id('Add alarm').click()
+        self.driver.find_element_by_accessibility_id('4').click()
+        self.driver.find_element_by_accessibility_id('30').click()
+        self.driver.find_element_by_id('android:id/button1').click()
 
-elemStopwatch = driver.find_elements_by_accessibility_id('Stopwatch')
-elemStopwatch[0].click()
-driver.implicitly_wait(1)
+        # Check that Alarm was added
+        self.assertTrue(self.driver.find_element_by_accessibility_id('4:30 PM'))
+
+        self.driver.implicitly_wait(2)
+
+        self.driver.find_element_by_id('com.google.android.deskclock:id/delete').click()
+        # self.assertEqual(self.driver.find_element_by_id('com.google.android.deskclock:id/snackbar_text').text, "Alarm deleted")
+        self.driver.implicitly_wait(2)
+
+    def test_clock_main(self):
+        self.driver.find_element_by_accessibility_id('Clock').click()
+
+        self.driver.implicitly_wait(2)
+
+    def test_clock_timer(self):
+        self.driver.find_element_by_accessibility_id('Timer').click()
+        self.assertEqual(self.driver.find_element_by_id('com.google.android.deskclock:id/timer_setup_time').text,
+                         "00h 00m 00s")
+        self.driver.implicitly_wait(2)
+
+    def test_clock_stopwatch(self):
+        self.driver.find_element_by_accessibility_id('Stopwatch').click()
+        self.assertEqual(self.driver.find_element_by_id('com.google.android.deskclock:id/stopwatch_time_text').text, "0")
+        self.assertEqual(self.driver.find_element_by_id('com.google.android.deskclock:id/stopwatch_hundredths_text').text,
+                         "00")
+        self.driver.implicitly_wait(2)
+
+
+if __name__ == "__main__":
+    unittest.main()
